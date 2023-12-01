1ef8a45a36cfbb92b0cfe1fbf3ad76175bf7ee49
EnzioKam <enziokamhh@gmail.com>
2023-12-01 17:27:02 +0800

    Revert some portal settings

--- configuration.nix
+++ configuration.nix
@@ -181,12 +181,19 @@
     };
   };
 
-  xdg.portal.config = {
-    common = {
-      default = [
-        "wlr"
-        "gtk"
-      ];
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ]
+    config = {
+      common = {
+        default = [
+          "wlr"
+          "gtk"
+        ];
+      };
     };
   };
 
