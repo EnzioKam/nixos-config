ae6e16e0eee3990a99a46535bfd9a5c88760bd7c
EnzioKam <enziokamhh@gmail.com>
2026-09-20 21:34:02 +0800

    Add utilities

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -139,6 +139,9 @@
 
   programs.steam.enable = true;
   programs.gamemode.enable = true;
+  services.lact.enable = false;
+  hardware.amdgpu.overdrive.enable = true;
+  programs.coolercontrol.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
